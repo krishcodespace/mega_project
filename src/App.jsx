e45aad8f5b6337c4492d9d769725befc8ef14026
krishcodespace <kanokakaidiya47@gmail.com>
2023-10-04 20:32:05 +0530

    intial setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const apivalue = import.meta.env.VITE_KEY;
+console.log(apivalue);
+
+function App() {
+
+  
+  return (
+    <>
+<div>
+  <h4>react mega projec</h4>
+</div>
+    </>
+  )
+}
+
+export default App
